6c69f73b9d13c45ada8c02ebab0fac789d919157
Keith Curtis <keithcu@gmail.com>
2026-03-06 10:45:31 -0500

    docs cleanup

--- run_pytest_csv.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-
-res = subprocess.run(["pytest", "tests/legacy/test_csv_import_logic.py"], env={"PYTHONPATH": "."})
-sys.exit(res.returncode)
--- run_pytest_formula.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-
-res = subprocess.run(["pytest", "tests/legacy/test_formula_parsing.py"], env={"PYTHONPATH": "."})
-sys.exit(res.returncode)
