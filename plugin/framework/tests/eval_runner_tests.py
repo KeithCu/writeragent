28c61931cef94cb89d144d931b7cf7afabfc6426
Keith Curtis <keithcu@gmail.com>
2026-03-07 00:53:04 -0500

    move tests to test directory

--- plugin/framework/tests/eval_runner_tests.py
+++ plugin/framework/tests/eval_runner_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-from plugin.framework.eval_runner import EvalRunner
+from plugin.framework.tests.eval_runner import EvalRunner
 
 class TestEvalRunner(unittest.TestCase):
     def setUp(self):
